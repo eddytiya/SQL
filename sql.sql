91eb26730bd76586ab38f1d74e7303512a2380f6
aditya pathak <102594671+eddytiya@users.noreply.github.com>
2024-05-02 15:26:17 +0530

    Add files via upload

--- sql.sql
+++ sql.sql
@@ -1,16 +1,43 @@
 CREATE DATABASE temp1;
 create database temp2;
+DROP DATABASE college;
 
-CREATE DATABASE college;
+CREATE DATABASE IF NOT EXISTS college;
 
 drop database temp1;
 DROP DATABASE temp2;#deleting database
 
 USE college;#using the created database college
 
+#creating a table
 CREATE TABLE student(
    id INT PRIMARY KEY,
    name VARCHAR(50),
    age INT NOT NULL
 );
+
+#adding data into the table
+
+INSERT INTO student VALUES(1,"ADITYA",21);
+INSERT INTO student VALUES(2,"VIRAT",35);
+
+#PRINTING TABLE
+SELECT * FROM student;
+
+#DB REALATED QUERIES
+DROP DATABASE IF EXISTS COMPANY;
+
+SHOW DATABASES;
+
+SHOW TABLES;
+
+#TABLE RELATED QUERIES
+
+
+
+
+
+
+
+
 
